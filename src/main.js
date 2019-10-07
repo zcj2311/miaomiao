1bf01aa40d3c96d93e26d4a750df59a536915102
zcj2311 <2311366314@qq.com>
2019-10-07 17:38:43 +0800

    add createComponents

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './routers/index'
+import store from './stores/index'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
