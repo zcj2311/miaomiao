a36885812f59c8dd805f53df65d8e49f84d2771d
zcj2311 <2311366314@qq.com>
2019-10-11 20:28:21 +0800

    add detailPage

--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -17,9 +17,9 @@
                 </li> -->
                 <li class="pullDown">{{pullDownMsg}}</li>
                 <li v-for="item in movieList" :key="item.id">
-                    <div class="pic_show" @tap="handleToDetail"><img :src="item.img | setWH('128.180')"></div>
+                    <div class="pic_show" @tap="handleToDetail(item.id)"><img :src="item.img | setWH('128.180')"></div>
                     <div class="info_list">
-                        <h2>{{item.nm}}<img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
+                        <h2 @tap="handleToDetail(item.id)">{{item.nm}}<img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
                         <p>观众评 <span class="grade">{{item.sc}}</span></p>
                         <p>主演: {{item.star}}</p>
                         <p>{{item.showInfo}}</p>
@@ -91,8 +91,8 @@ export default {
         })
     },
     methods:{//点击事件方法
-        handleToDetail(){
-
+        handleToDetail(movieId){
+            this.$router.push('/movie/detail/1/'+movieId);
         },
         handleToScroll(pos){
             if(pos.y>30){
